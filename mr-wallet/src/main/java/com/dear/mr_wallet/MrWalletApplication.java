6afacdbf6a4b5305990db8e65501c60168b378a1
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2024-06-19 17:02:54 +0900

    Add files via upload

---
+++ mr-wallet/src/main/java/com/dear/mr_wallet/MrWalletApplication.java
@@ -0,0 +1,13 @@
+package com.dear.mr_wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrWalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrWalletApplication.class, args);
+	}
+
+}
