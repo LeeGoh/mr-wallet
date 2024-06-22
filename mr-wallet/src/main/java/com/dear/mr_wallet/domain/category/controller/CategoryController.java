854773ae4a674f4cbb3a360e7a745cd140f74b88
LeeGoh <107545016+LeeGoh@users.noreply.github.com>
2024-06-22 16:56:34 +0900

    feat: category create, edit, delete (#11)
    Co-authored-by: LeeGoh <“gyuleegoh@gmail.com”>

--- mr-wallet/src/main/java/com/dear/mr_wallet/domain/category/controller/CategoryController.java
+++ mr-wallet/src/main/java/com/dear/mr_wallet/domain/category/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.dear.mr_wallet.domain.category.controller;
 
+import com.dear.mr_wallet.domain.category.dto.PostCategoryDto;
+import com.dear.mr_wallet.domain.category.service.CategoryService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +10,18 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 public class CategoryController {
+    private final CategoryService categoryService;
+
     @PostMapping("/category")
-    public ResponseEntity createCategory() {
+    public ResponseEntity createCategory(@RequestBody PostCategoryDto post) {
+        categoryService.createCategory(post);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PatchMapping("/category/{category-id}")
-    public ResponseEntity editCategory(@PathVariable("category-id") Long categoryId) {
+    public ResponseEntity editCategory(@RequestBody PostCategoryDto patch,
+                                       @PathVariable("category-id") Long categoryId) {
+        categoryService.editCategory(patch, categoryId);
         return ResponseEntity.ok().build();
     }
 
@@ -25,6 +32,7 @@ public class CategoryController {
 
     @DeleteMapping("category/{category-id}")
     public ResponseEntity deleteCategory(@PathVariable("category-id") Long categoryId) {
+        categoryService.deleteCategory(categoryId);
         return ResponseEntity.noContent().build();
     }
 }
--- mr-wallet/src/main/java/com/dear/mr_wallet/domain/category/service/CategoryDbService.java
+++ mr-wallet/src/main/java/com/dear/mr_wallet/domain/category/service/CategoryDbService.java
@@ -2,11 +2,27 @@ package com.dear.mr_wallet.domain.category.service;
 
 import com.dear.mr_wallet.domain.category.entity.Category;
 import com.dear.mr_wallet.domain.category.repository.CategoryRepository;
+import com.dear.mr_wallet.global.exception.BusinessLogicException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static com.dear.mr_wallet.global.exception.ExceptionCode.CATEGORY_NOT_FOUND;
 
 @Service
 @RequiredArgsConstructor
 public class CategoryDbService {
     private final CategoryRepository categoryRepository;
+
+    public Category ifExistsReturnCategory(Long categoryId) {
+        return categoryRepository.findById(categoryId)
+                .orElseThrow(() -> new BusinessLogicException(CATEGORY_NOT_FOUND));
+    }
+
+    public void saveCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+    public void removeCategory(Category category) {
+        categoryRepository.delete(category);
+    }
 }
